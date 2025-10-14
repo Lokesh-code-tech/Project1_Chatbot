477365223dd66630b4a1d304b96b5ced7b5d576b
Lokesh Bansal <bansal.lokesh89@gmail.com>
2025-10-14 23:06:27 +0530

    sixth

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,6 +1,8 @@
-from pydantic_ai import Agent
 
-chatbot_agent = Agent('openai:gpt-5-nano',system_prompt="You answer concise.")
+from pydantic_ai import Agent
+def chatbot():
+    chatbot_agent = Agent('openai:gpt-5-nano',system_prompt="You answer concise.")
+    return chatbot_agent
 
 # result1 = chatbot_agent.run_sync("My name is Lokesh")
 # result2 = chatbot_agent.run_sync("What is my name?",message_history=result1.all_messages())
